98d4ce63cf390102e15722e6bad21c52c2829913
Anto Jebikshan <dantojebi@gmail.com>
2025-02-05 11:44:30 +0530

    test

---
+++ src/Concepts/ExceptionExample.java
@@ -0,0 +1,13 @@
+package Concepts;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } finally {
+            System.out.println("This will always execute.");
+        }
+    }
+}
