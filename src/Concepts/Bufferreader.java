73e36d1356b7834e9d5e818616fa66120a35fe6c
Anto Jebikshan <dantojebi@gmail.com>
2025-02-12 10:38:09 +0530

    update

--- src/Concepts/Bufferreader.java
+++ src/Concepts/Bufferreader.java
@@ -6,13 +6,19 @@ public class Bufferreader {
     public static void main(String[] args) throws IOException {
 
        //InputStreamReader ISR = new InputStreamReader(System.in); // or
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("enter Message");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("enter Message");
+//
+//        String Lines = br.readLine();
+//        System.out.println(Lines);
+        InputStreamReader ins = new InputStreamReader(System.in);
+        BufferedReader brr = new BufferedReader(ins);
 
-        String Lines = br.readLine();
-        System.out.println(Lines);
+        System.out.println("enter your name : " );
+        String name = brr.readLine();
+        System.out.println(name);
 
-        br.close();
+        brr.close();
 
 
     }
---
+++ src/Concepts/Multiline_Inputs.java
@@ -0,0 +1,25 @@
+package Concepts;
+
+import java.util.Scanner;
+
+public class Multiline_Inputs {
+    public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        System.out.println("enter your message : ");
+
+        while(true){
+            String getInp = inp.nextLine();
+            if(getInp.isEmpty()){
+                break;
+            }
+
+            sb.append(getInp).append("\n");
+        }
+
+        inp.close();
+
+        System.out.println(sb.toString());
+
+    }
+}
