8749abc6a34556458c0360951f09fcbd554902a0
Anto Jebikshan <dantojebi@gmail.com>
2025-02-14 20:37:26 +0530

    update

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Arrays {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arr = {32,34,34,54};
+        int[] arr1 = new int[45];
+        for(int num : arr1){
+            System.out.println(num);
+        }
+    }
+}
