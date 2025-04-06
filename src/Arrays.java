8c73af737f7ac3ec98620a2b89a6762231c2219c
Anto Jebikshan <dantojebi@gmail.com>
2025-04-06 22:33:33 +0530

    changes

--- src/Arrays.java
+++ src/Arrays.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Arrays {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        int a = 10;
         int[] arr = {32,34,34,54};
         int[] arr1 = new int[45];
         for(int num : arr1){
