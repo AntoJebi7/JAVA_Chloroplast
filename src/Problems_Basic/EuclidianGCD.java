c65da8b3fc92e9da0eef5ddb5b5a47d8f5f0bdd8
Anto Jebikshan <dantojebi@gmail.com>
2025-04-07 22:02:10 +0530

    changes

---
+++ src/Problems_Basic/EuclidianGCD.java
@@ -0,0 +1,25 @@
+package Problems_Basic;
+
+import java.util.Scanner;
+
+public class EuclidianGCD {
+    public static int GCD(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return GCD(b,a%b);  // a = b , b = remainder
+    }
+    public static int LCM(int a, int b, int c){
+        // ( a * b ) / gcd (a,b)
+         return (a*b/c);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(GCD(21,300) );
+
+        // also find lcm with this
+        System.out.println(LCM(a,b,GCD(a,b)));
+    }
+}
